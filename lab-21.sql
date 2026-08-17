13639ef82ceb751aeaf10059a2638cab941b515d
mayankpathar <mayankpathar49@gmail.com>
2026-08-17 10:40:36 +0530

    Add lab-21.sql with SQL queries for student performance analysis and ranking

---
+++ lab-21.sql
@@ -0,0 +1,88 @@
+--Part – A:
+
+--1. Display all students whose SPI is greater than 8.
+ WITH SPI_GREATER_THAN AS(
+ SELECT * FROM student 
+ WHERE SPI > 8
+ )
+ SELECT * FROM SPI_GREATER_THAN
+ 
+ 
+--2. Display average SPI of all students.
+ WITH SPI_AVERAGE AS(
+ SELECT STDID,SNAME,AVG(SPI) AS AVGSPI FROM student
+ GROUP BY STDID,SNAME
+ )
+ SELECT * FROM SPI_AVERAGE
+
+
+--3. Display total number of students in each branch.
+ WITH EACH_BRANCH AS(
+ SELECT BRANCH,COUNT(BRANCH) AS AVGSPI FROM student
+ GROUP BY BRANCH
+ )
+ SELECT * FROM EACH_BRANCH
+
+--4. Display students who belong to RAJKOT city.
+ WITH Display_RAJKOT AS(
+ SELECT * FROM student
+ WHERE CITY='RAJKOT'
+ )
+ SELECT * FROM Display_RAJKOT
+
+--5. Find branch names that appear more than once.
+ WITH APPER_MORE_THAN_ONE AS(
+ SELECT BRANCH,COUNT(STDID) AS COUNT_BRANCH FROM student
+ GROUP BY BRANCH
+ )
+ SELECT * FROM APPER_MORE_THAN_ONE
+ WHERE COUNT_BRANCH>1
+
+--6. Display row number for each student.
+WITH Display_row_number AS(
+ SELECT SNAME,
+ ROW_NUMBER() OVER(ORDER BY STDID DESC) AS ROWNUMBER
+ FROM student
+ 
+ )
+ SELECT * FROM Display_row_number
+
+--7. Display top 3 students based on SPI.
+WITH SPI_TOP_3 AS(
+ SELECT STDID,SNAME,CITY,SPI,BRANCH,
+ DENSE_RANK() OVER(ORDER BY STDID DESC) AS ROWNUMBER
+ FROM student
+ )
+ SELECT TOP 3 * FROM SPI_TOP_3
+
+--8. Display students having maximum SPI.
+
+
+--9. Display students having minimum SPI.
+
+
+--10. Display branch -wise rank of students.
+
+--Part – B:
+
+--11. Display students SPI average belonging to Computer branch.
+
+--12. Display students whose SPI is greater than average SPI of his/her branch.
+
+--13. Display branch having more than 2 students.
+
+--14. Display branches having average SPI between 7 and 9
+
+--15. Display students whose SPI is lower than overall average SPI.
+
+--Part – C:
+
+--16. Display branches having exactly one student.
+
+--17. Display branch having highest average SPI.
+
+--18. Display branch having lowest average SPI.
+
+--19. Display students whose SPI is lower than branch average SPI.
+
+--20. Display branches having maximum number of students.
